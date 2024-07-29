d33b23bd3c5f92193426b5c8f0f46255971a9b84
rhj-git <rakibulhasanjoy009@gmail.com>
2024-07-29 22:09:55 +0600

    home page

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -24,8 +24,8 @@
             $('.navbar').removeClass('sticky-top shadow-sm');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -35,7 +35,9 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -45,7 +47,9 @@
         $('.progress .progress-bar').each(function () {
             $(this).css("width", $(this).attr("aria-valuenow") + '%');
         });
-    }, {offset: '80%'});
+    }, {
+        offset: '80%'
+    });
 
 
     // Facts counter
@@ -62,44 +66,39 @@
         margin: 25,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            992:{
-                items:2
+            992: {
+                items: 2
             }
         }
     });
 
     // Team carousel
-    var teamItems = $('.team-item').length;
-        if (teamItems > 3) {
-            $(".owl-carousel").owlCarousel({
-                loop: true,
-                margin: 10,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    768: {
-                        items: 2
-                    },
-                    992: {
-                        items: 3
-                    }
-                }
-            });
-        } else {
-            // If there are 3 or fewer items, do not initialize the carousel
-            $('.owl-carousel').addClass('row g-4');
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 15,
+        dots: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
         }
-    
+    });
 
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
@@ -110,8 +109,9 @@
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({
+            filter: $(this).data('filter')
+        });
     });
-    
-})(jQuery);
 
+})(jQuery);
